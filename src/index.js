01f5e17495d0f6f0661b6ecdb96d07e2edc1bbea
Espen Norderud <espen42@gmail.com>
2017-02-27 11:53:00 +0100

    Functional / non-mutating, and closer to the ducks convention.

--- src/index.js
+++ src/index.js
@@ -35,75 +35,78 @@ export const makeReducer = (reducerTable, initialState) =>
     };
 
 
+
+
+let actionNum = 0;
+const getActionType = (prefix, actionName) =>
+    (prefix != null ? prefix : "") +
+    ((actionName == null || actionName === "") ?
+        "" + (actionNum++) :
+        actionName);
+
+
+const makeActionCreator = (actionType, actionArgumentNames = []) => (...args) => {
+    const action = {type: actionType};
+    actionArgumentNames.forEach( (key, idx) => { action[key] = args[idx]; } );
+    console.log("New reducer action:", action);
+    return action;
+};
+
+
+
+const buildActionCreatorAndReducerMap = (prefix, actionAndReducerMap, checkAndWarn, logProduced) => {
+    const actionCreatorMap = {};
+    const reducerMap = {};
+    const typeMap = {};
+
+    Object.keys(actionAndReducerMap).forEach( actionName => {
+
+        const actionType = getActionType(prefix, actionName);
+        let reducerFunction = actionAndReducerMap[actionName];
+        const actionArgumentNames = getReducerArgNames(reducerFunction, actionName) || [];
+
+        if (checkAndWarn) {
+            check(actionType, actionArgumentNames, reducerFunction);
+        }
+
+        actionCreatorMap[actionName] = makeActionCreator(actionType, actionArgumentNames);
+        reducerMap[actionName] = reducerFunction;
+        typeMap[actionName] = actionType;
+
+        if (logProduced) {
+            console.log("\nActionCreator: getActionCreators()." + actionName + "(" + actionArgumentNames.join(", ") + ")");
+            console.log("\tType: getTypes()." + actionName + " = '" + actionType + "'");
+        }
+    });
+
+    return [actionCreatorMap, reducerMap, typeMap];
+};
+
+
 /**
  *  Creates an action-actioncreator-reducer unified complex: a redux duck.
  */
 class DuckFactory {
-    constructor(actionTypePrefix, initialState, actionAndReducerMapOrMaps, checkAndWarn = true) {
-        this._prefix = actionTypePrefix;
-        this._initialState = initialState;
-        this._actionNum = 0;
-        this._reducerTable = {};
-        this._checkAndWarn = checkAndWarn;
-
-        this._makeActionCreator = this._makeActionCreator.bind(this);
-        this._mutateActionAndReducerMap = this._mutateActionAndReducerMap.bind(this);
-
-
-        if (actionAndReducerMapOrMaps != null && (typeof actionAndReducerMapOrMaps === 'object')) {
-            if (!Array.isArray(actionAndReducerMapOrMaps)) {
-                actionAndReducerMapOrMaps = [actionAndReducerMapOrMaps];
-            }
-            actionAndReducerMapOrMaps.forEach(actionAndReducerMap => {
-                this._mutateActionAndReducerMap(actionAndReducerMap);
-            });
+    constructor(actionTypePrefix, initialState, actionAndReducerMap, checkAndWarn = true, logProduced = false) {
+        if (actionAndReducerMap == null || (typeof actionAndReducerMap !== 'object')) {
+            throw Error("Can't create a duck without actionAndReducerMap: action creator name --> reducer function");
         }
 
-        this.createReducer = this.createReducer.bind(this);
+        const [actionCreatorMap, reducerMap, typeMap] = buildActionCreatorAndReducerMap(
+            actionTypePrefix, actionAndReducerMap, checkAndWarn, logProduced);
+
+        this._actionCreators = actionCreatorMap;
+        this._reducer = makeReducer(reducerMap, initialState);
+        this._types = typeMap;
+
+        this.getReducer = this.getReducer.bind(this);
+        this.getActionCreators = this.getActionCreators.bind(this);
+        this.getTypes = this.getTypes.bind(this);
     }
 
-
-    _mutateActionAndReducerMap(actionAndReducerMap) {
-        Object.keys(actionAndReducerMap).forEach( actionName => {
-            let reducerFunction = actionAndReducerMap[actionName];
-
-            const actionArgumentNames = getReducerArgNames(reducerFunction, actionName);
-
-            actionAndReducerMap[actionName] = this._makeActionCreator(
-                actionName, actionArgumentNames, reducerFunction);
-        });
-    }
-
-
-    _makeActionCreator(actionName, actionArgumentNames = [], reducerFunction) {
-        const actionType = this._prefix + "_" + ((actionName == null || actionName === "") ?
-            "" + (this._actionNum++) :
-            actionName);
-
-        if (this._checkAndWarn) {
-            check(actionType, actionArgumentNames, reducerFunction);
-        }
-
-        const actionCreator = (...args) => {
-            const action = {type: actionType};
-            actionArgumentNames.forEach( (key, idx) => { action[key] = args[idx]; } );
-            console.log("New reducer action:", action);
-            return action;
-        };
-
-        console.log("\nReducer actionCreator:", actionName + "(" + actionArgumentNames.join(", ") + ")");
-        console.log("\tactionType:", actionType);
-
-        this._reducerTable[actionType] = reducerFunction;
-
-        return actionCreator;
-    }
-
-
-    createReducer() {
-        return makeReducer(this._reducerTable, this._initialState);
-    }
-
+    getReducer() { return this._reducer; }
+    getActionCreators() { return this._actionCreators; }
+    getTypes() { return this._types; }
 
 }
 export default DuckFactory;
--- test/duckfactory_spec.js
+++ test/duckfactory_spec.js
@@ -2,37 +2,59 @@ import { expect } from 'chai';
 
 import DuckFactory from '../src';
 
-const duckFactory = new DuckFactory("DKTST", {}, true);
+const duckFactory = new DuckFactory("duck/test/", {}, {
+    setHey: (state, {ya}) => ({hey: ya}),
+    doubleHey: (state) => ({hey: state.hey * 2}),
+    insertSecondAndThirdAsWhoaYeah: (state, {first, second, third}) => {
+        return {
+            ...state,
+            hey: first,
+            whoa: second,
+            yeah: "Yeah: " + third,
+        };
+    },
+}, true, true);
 
 describe("DuckFactory", ()=>{
-    describe("._makeActionCreator", ()=>{
+    describe(".getActionCreators().action", ()=>{
         it("uses the actioncreator to create actions as expected, with arguments", ()=>{
-            const actionCreator = duckFactory._makeActionCreator(
-                "ACTION1",
-                ["woop", "wheee", "ohyes"],
-                (state, {woop, wheee, ohyes})=>{ console.log("Gosh darn it to heck!!!!"); },
-            );
 
-            const action = actionCreator(2, 42, 666);
-            expect(action).to.deep.equal({
-                type: "DKTST_ACTION1",
-                woop: 2,
-                wheee: 42,
-                ohyes: 666,
+            const action1 = duckFactory.getActionCreators().setHey(2, 42, 777);
+
+            expect(action1).to.deep.equal({
+                type: "duck/test/setHey",
+                ya: 2,
+            });
+
+            const action2 = duckFactory.getActionCreators().doubleHey(2, 42, 777);
+
+            expect(action2).to.deep.equal({
+                type: "duck/test/doubleHey",
+            });
+
+            const action3 = duckFactory.getActionCreators().insertSecondAndThirdAsWhoaYeah(2, 42, 777);
+
+            expect(action3).to.deep.equal({
+                type: "duck/test/insertSecondAndThirdAsWhoaYeah",
+                first: 2,
+                second: 42,
+                third: 777,
             });
         });
 
 
-        it("tolerates excessive number of action arguments, compared to the actionCreator actionArgumentNames", ()=>{
-            const actionCreator = duckFactory._makeActionCreator(
-                "ACTION2",
-                ["woop", "wheee", "ohyes"],
-                (state, {woop, wheee, ohyes})=>{ console.log("Awright"); }
-            );
+        /*it("tolerates excessive number of action arguments, compared to the actionCreator actionArgumentNames", ()=>{
+            const duckFactory = new DuckFactory("duck/test/", {}, {
+                ACTION2: (state, {woop, wheee, ohyes}) => {
+                    console.log("Okay");
+                    return state;
+                },
+            }, true, true);
 
-            const action = actionCreator(2, 42, 99, 47, 101);
+
+            const action = duckFactory.getActionCreators().ACTION2(2, 42, 99, 47, 101);
             expect(action).to.deep.equal({
-                type: "DKTST_ACTION2",
+                type: "duck/test/ACTION2",
                 woop: 2,
                 wheee: 42,
                 ohyes: 99,
@@ -40,11 +62,10 @@ describe("DuckFactory", ()=>{
         });
     });
 
-    describe("Construction with actionAndReducerMap", ()=>{
-        it("Takes input action maps ( actionName --> reducerFunction ) and replaces the value in the actionmap with" +
+    describe("Construction with an actionAndReducerMap", ()=>{
+        it("Takes input action map ( actionName --> reducerFunction ) and replaces the value in the actionmap with" +
             "an appropriate action creator", ()=>{
-
-            const actions1 = {
+            const duckFactory = new DuckFactory("duck/test/", {}, {
                 setHey: (state, {ya}) => ({hey: ya}),
                 doubleHey: (state) => ({hey: state.hey * 2}),
                 insertWhoaYeah: (state, {whoa, yeah}) => {
@@ -54,7 +75,9 @@ describe("DuckFactory", ()=>{
                         yeah: "Yeah: " + yeah,
                     };
                 },
-            };
+            }, true, true);
+
+            const actions1 = ;
 
             new DuckFactory("DKTST2", {hey: 5}, actions1);
 
@@ -68,6 +91,6 @@ describe("DuckFactory", ()=>{
         it("tolerates excessive number of action arguments, compared to the actionCreator actionArgumentNames", ()=>{
 
             //const duckFactory3 = new DuckFactory("DKTST3", {}, true);
-        });
+        });//*/
     });
 });
